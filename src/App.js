5396fbbe234c55ac091364b95ef1ce117d8c44b8
Abhishek Kumar <abhisheksmscollege@gmail.com>
2023-07-15 19:48:31 +0530

    first signup form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FormValidation from "./Components/FormValidation";
+
+
+const App = () => {
+
+    return(
+        <div>
+           
+            <FormValidation />
+        </div>
+    )
+}
+
+export default App;
